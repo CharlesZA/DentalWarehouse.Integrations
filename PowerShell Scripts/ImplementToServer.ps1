c2728d82de52b408745e45ef5735cc1df02314e3
CharlesZA <30626022+CharlesZA@users.noreply.github.com>
2018-11-16 13:57:37 +0200

    powershell

---
+++ PowerShell Scripts/ImplementToServer.ps1
@@ -0,0 +1,3 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+
+Publish-NAVApp -ServerInstance "DynamicsNAV110" -Path "C:\...\RIVERINE TECHNOLOGY SERVICES_DentalWarehouse Integrations_11.0.21441.0.app" -SkipVerification
