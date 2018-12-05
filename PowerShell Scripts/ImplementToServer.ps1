44526f8e39a7a19f468706098e7eb3e0f4099114
pseethal <pseethal@yahoo.com>
2018-12-05 09:06:34 +0200

    Re-Publish

--- PowerShell Scripts/ImplementToServer.ps1
+++ PowerShell Scripts/ImplementToServer.ps1
@@ -1,3 +1,10 @@
 ﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
 
+
+Publish-NAVApp -ServerInstance "DynamicsNAV110" -Path "C:\Temp\RIVERINE TECHNOLOGY SERVICES_DentalWarehouse Integrations_11.0.21441.0.app" -SkipVerification
+
 Publish-NAVApp -ServerInstance "DynamicsNAV110" -Path "C:\...\RIVERINE TECHNOLOGY SERVICES_DentalWarehouse Integrations_11.0.21441.0.app" -SkipVerification
+
+
+
+ Unpublish-NAVApp -ServerInstance BC -Path '.\Proseware SmartApp.app'
